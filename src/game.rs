c0071fd90ec909ffadc4b30b56b1f50cd6e4b09d
Eduardo Canellas de Oliveira <eduardo.canellas@bemobi.com>
2022-04-03 16:07:37 -0300

    add pads movement

--- src/game.rs
+++ src/game.rs
@@ -1,12 +1,18 @@
 use bevy::prelude::*;
 
-use self::{pad::pad_bundle, scoreboard::scoreboard};
+use self::{
+    movement::{movement_system, MoveControls},
+    pad::pad_bundle,
+    scoreboard::scoreboard,
+};
 
 pub struct GamePlugin;
 
 impl Plugin for GamePlugin {
     fn build(&self, app: &mut App) {
-        app.add_startup_system(hello).add_startup_system(setup);
+        app.add_startup_system(hello)
+            .add_startup_system(setup)
+            .add_system(movement_system);
     }
 }
 
@@ -20,8 +26,24 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
     let font = asset_server.load("fonts/ComicNeue-Regular.ttf");
     commands.spawn_bundle(scoreboard(font));
 
-    commands.spawn_batch([pad_bundle(-500.0, 0.0), pad_bundle(500.0, 0.0)]);
+    commands
+        .spawn_bundle(pad_bundle(500.0, 0.0))
+        .insert(MoveControls {
+            up: KeyCode::Up,
+            down: KeyCode::Down,
+            left: KeyCode::Left,
+            right: KeyCode::Right,
+        });
+    commands
+        .spawn_bundle(pad_bundle(-500.0, 0.0))
+        .insert(MoveControls {
+            up: KeyCode::W,
+            down: KeyCode::S,
+            left: KeyCode::A,
+            right: KeyCode::D,
+        });
 }
 
+mod movement;
 mod pad;
 mod scoreboard;
---
+++ src/game/movement.rs
@@ -0,0 +1,32 @@
+use bevy::prelude::*;
+
+#[derive(Debug, Component)]
+pub struct MoveControls {
+    pub up: KeyCode,
+    pub down: KeyCode,
+    pub left: KeyCode,
+    pub right: KeyCode,
+}
+
+pub fn movement_system(
+    input: Res<Input<KeyCode>>,
+    mut query: Query<(&mut Transform, &MoveControls)>,
+) {
+    macro_rules! control {
+        ($input:ident, $controls:ident, $transform:ident, $speed:ident, $control:ident) => {
+            if $input.pressed($controls.$control) {
+                let $control = $transform.$control();
+                $transform.translation += $control * $speed;
+            }
+        };
+    }
+
+    let speed = 10.;
+
+    for (mut transform, controls) in query.iter_mut() {
+        control!(input, controls, transform, speed, up);
+        control!(input, controls, transform, speed, down);
+        control!(input, controls, transform, speed, left);
+        control!(input, controls, transform, speed, right);
+    }
+}
