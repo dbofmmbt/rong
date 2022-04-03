5f050e5437955d2f9393a2f48dabe5b0eb31ea38
Eduardo Canellas de Oliveira <eduardo.canellas@bemobi.com>
2022-04-03 15:14:39 -0300

    add static scoreboard

--- src/game.rs
+++ src/game.rs
@@ -1,6 +1,6 @@
 use bevy::prelude::*;
 
-use self::pad::pad_bundle;
+use self::{pad::pad_bundle, scoreboard::scoreboard};
 
 pub struct GamePlugin;
 
@@ -14,10 +14,14 @@ fn hello() {
     info!("Hello, world!");
 }
 
-fn setup(mut commands: Commands) {
+fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
     commands.spawn_bundle(OrthographicCameraBundle::new_2d());
+
+    let font = asset_server.load("fonts/ComicNeue-Regular.ttf");
+    commands.spawn_bundle(scoreboard(font));
 
     commands.spawn_batch([pad_bundle(-500.0, 0.0), pad_bundle(500.0, 0.0)]);
 }
 
 mod pad;
+mod scoreboard;
---
+++ src/game/scoreboard.rs
@@ -0,0 +1,21 @@
+use bevy::prelude::*;
+
+use crate::util::default;
+
+pub fn scoreboard(font: Handle<Font>) -> impl Bundle {
+    let style = TextStyle {
+        font,
+        font_size: 50.,
+        color: Color::WHITE,
+    };
+    let alignment = TextAlignment {
+        horizontal: HorizontalAlign::Center,
+        ..default()
+    };
+
+    Text2dBundle {
+        text: Text::with_section("Scoreboard", style, alignment),
+        transform: Transform::from_xyz(0., 320., 0.),
+        ..default()
+    }
+}
