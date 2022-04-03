5868b560b07802f55c40aa5b97e4464ddc261e33
Eduardo Canellas de Oliveira <eduardo.canellas@bemobi.com>
2022-04-03 15:01:25 -0300

    configure window props

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,16 @@
 use bevy::prelude::*;
 use game::GamePlugin;
+use util::default;
 
 fn main() {
     App::new()
+        .insert_resource(WindowDescriptor {
+            title: "RONG".to_string(),
+            width: 1280.,
+            height: 720.,
+            resizable: false,
+            ..default()
+        })
         .add_plugins(DefaultPlugins)
         .add_plugin(GamePlugin)
         .run();
